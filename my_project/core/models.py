d1a6ff2d8241e2fec496385d541bb5d739055b05
blandskron <bastianlandskronfreelanceer@gmail.com>
2024-11-17 15:28:18 -0300

    update

---
+++ my_project/core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
